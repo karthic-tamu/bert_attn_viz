68b7e8b824e4c4422f7810ae4d16f7e20ca23bad
Karthic Madanagopal <79726805+karthic-tamu@users.noreply.github.com>
2022-05-19 19:46:42 -0500

    Update optimization.py

--- bert_attn_viz/optimization.py
+++ bert_attn_viz/optimization.py
@@ -19,7 +19,8 @@ from __future__ import division
 from __future__ import print_function
 
 import re
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu):
